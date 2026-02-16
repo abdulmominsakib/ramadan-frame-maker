94f43712e9734c2baa00ce56779b5adbee1fe7b9
abdulmominsakib <mdmomin322@gmail.com>
2026-02-17 01:01:02 +0600

    Updates

--- _next/static/wo_dNUN1388XeB7rRkcJM/_buildManifest.js
+++ _next/static/wo_dNUN1388XeB7rRkcJM/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ramadan-frame-maker//_next/:path+",
+        "destination": "/ramadan-frame-maker/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
